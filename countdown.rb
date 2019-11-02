4ec59817f8826bc82a33ad6d53b10d489e6d5470
Joseph Miller <jdworldtv@gmail.com>
2019-11-02 05:13:50 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,14 +1,11 @@
 
-def countdown_with_sleep
-  
-  x = 0 
-  while  x < 10 
-  
-   puts "#{x} is less than 10"
- until.time.now
-  sleep 1 seconds until time.now 
-puts = phrase 
-
-phrase = "HAPPY NEW YEAR!"
- 
+def countdown(number)
+ x =1 
+ while x < 10
+  puts "#{10} SECOND(S)!" "#{9} SECOND(S)!""#{8} SECOND(S)!" " #{7} SECOND(S)!" "#{6} SECOND(S)!"
+  "#{5} SECOND(S)!" "#{4} SECOND(S)!" "#{3} SECOND(S)!" "#{2} SECOND(S)!""#{1} SECOND(S)!"
+  "#{0} SECOND(S)!"
+    puts "HAPPY NEW YEAR!"
+  x +=1
+  end
 end
